2af9c4fd1358f851a8ec2f69a356fc56c0e35ee2
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-16 17:21:30 +0600

    few changes

--- BankTable/src/main/java/com/BankTable/BankTable/controller/CommandLineApp.java
+++ BankTable/src/main/java/com/BankTable/BankTable/controller/CommandLineApp.java
@@ -2,20 +2,25 @@ package com.BankTable.BankTable.controller;
 
 import com.BankTable.BankTable.model.User;
 import com.BankTable.BankTable.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 @Component
 public class CommandLineApp implements CommandLineRunner {
 
 
+    @Autowired
     private final UserRepository userRepo;
+    private final User user;
 
-    public CommandLineApp(UserRepository userRepo) {
+    public CommandLineApp(UserRepository userRepo, User user) {
         this.userRepo = userRepo;
+        this.user = user;
     }
 
     @Override
@@ -42,7 +47,7 @@ public class CommandLineApp implements CommandLineRunner {
                 case 1 -> addUser();
                 case 2 -> findUser(scanner);
                 case 3 -> showAllUsers();
-                case 4 -> updateUsers();
+                case 4 -> updateUsers(scanner);
                 case 5 -> deleteUser(scanner);
                 case 6 -> {
                     System.out.println("You have Existing... pop");
@@ -87,21 +92,51 @@ public class CommandLineApp implements CommandLineRunner {
         System.out.println("User deleted");
     }
 
-    private void updateUsers() {
+    // to update the specific user
+    private void updateUsers(Scanner scanner) {
+//        User user = new User();
+
+        System.out.print("Choose client id you like to update: ");
+        long id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("You choose " + id + " ID to update");
+//        Optional<User> user = userRepo.findById(id);
+//        System.out.println(userRepo.findById(id));
+        userRepo.findById(id).ifPresentOrElse(
+
+                user -> System.out.println("User fond: " + user),
+
+                () -> System.out.println("User not found with ID: " + id)
+
+        );
+        System.out.println("Update your model " + id);
+        System.out.println("Enter account name :");
+        user.setAccount_name(scanner.nextLine());
+
+        System.out.println("Enter Loan account number :");
+        user.setLoan_account_number(scanner.nextLine());
+
+
+
+
+
     }
 
+    //it will show all the users in the database
     private void showAllUsers() {
         List<User> users = userRepo.findAll();
 
         if (users.isEmpty()){
             System.out.println("No user found");
         }else{
-            System.out.println("HEre ia all the data: ");
+            System.out.println("Here ia all the data: ");
             users.forEach(System.out::println);
         }
 
     }
 
+    // here you add the user using data
     private void addUser() {
+
     }
 }
--- BankTable/src/main/java/com/BankTable/BankTable/repo/UserRepository.java
+++ BankTable/src/main/java/com/BankTable/BankTable/repo/UserRepository.java
@@ -3,6 +3,9 @@ import com.BankTable.BankTable.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository <User,Long>{
 
+    //List<User> findAllById(long );
 }
